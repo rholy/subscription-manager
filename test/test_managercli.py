f9e305f5a81de742871ac83a35202f296f5395e0
Alex Wood <awood@redhat.com>
2013-10-22 14:16:15 -0400

    Tests should not rely on specific dictionary ordering.

--- test/test_managercli.py
+++ test/test_managercli.py
@@ -793,7 +793,7 @@ class TestOverrideCommand(TestCliProxyCommand):
         with capture() as out:
             self.cc._list(data, ['x', 'z'])
             output = out.getvalue()
-            self.assertTrue(re.search('Nothing is known about z', output))
+            self.assertTrue(re.search("Nothing is known about 'z'", output))
             self.assertTrue(re.search('Repository: x', output))
             self.assertTrue(re.search('\s+hello:\s+world', output))
 
@@ -807,7 +807,7 @@ class TestOverrideCommand(TestCliProxyCommand):
             {'contentLabel': 'y', 'name': 'c', 'value': 'd'},
         ]
         result = self.cc._add(repos, overrides)
-        self.assertEquals(expected, result)
+        self.assertTrue(dict_list_equals(expected, result))
 
     def test_remove_function(self):
         repos = ['x', 'y']
@@ -819,7 +819,7 @@ class TestOverrideCommand(TestCliProxyCommand):
             {'contentLabel': 'y', 'name': 'b'},
         ]
         result = self.cc._remove(repos, removes)
-        self.assertEquals(expected, result)
+        self.assertTrue(dict_list_equals(expected, result))
 
     def test_remove_all(self):
         repos = ['x', 'y']
@@ -828,7 +828,7 @@ class TestOverrideCommand(TestCliProxyCommand):
             {'contentLabel': 'y'},
         ]
         result = self.cc._remove_all(repos)
-        self.assertEquals(expected, result)
+        self.assertTrue(dict_list_equals(expected, result))
 
     def test_remove_all_with_no_repos_given(self):
         repos = []
