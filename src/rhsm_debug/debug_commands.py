ce0a0f857aea308eaa0f932c6ac6f5145f34ed28
Adrian Likins <alikins@redhat.com>
2013-12-12 18:49:41 -0500

    wip, objectify

--- src/rhsm_debug/debug_commands.py
+++ src/rhsm_debug/debug_commands.py
@@ -23,7 +23,6 @@ from datetime import datetime
 
 import subscription_manager.injection as inj
 import subscription_manager.managercli as managercli
-from subscription_manager.managercli import CliCommand
 from rhsm import ourjson as json
 from rhsm.config import initConfig
 
@@ -38,12 +37,12 @@ NOT_REGISTERED = _("This system is not yet registered. Try 'subscription-manager
 ASSEMBLE_DIR = '/var/spool/rhsm/debug'
 
 
-class SystemCommand(CliCommand):
+class SystemCommand(managercli.CliCommand):
 
     def __init__(self, name="system",
                  shortdesc=_("Assemble system information as a tar file or directory"),
                  primary=True):
-        CliCommand.__init__(self, name=name, shortdesc=shortdesc, primary=primary)
+        super(SystemCommand, self).__init__(name, shortdesc, primary)
 
         self.parser.add_option("--destination", dest="destination",
                                default="/tmp", help=_("the destination location of the result"))
@@ -62,85 +61,165 @@ class SystemCommand(CliCommand):
             print NOT_REGISTERED
             sys.exit(-1)
 
-        code = self._make_code()
-        assemble_path = self._get_assemble_dir()
-        content_path = os.path.join(assemble_path, code)
-        tar_path = os.path.join(assemble_path, "tar")
+        archive_config = DebugInfoArchiveConfig()
+        archiver = DebugInfoArchiver(archive_config)
+
+        if self.options.archive:
+            writer = DebugInfoTarWriter()
+        else:
+            writer = DebugInfoDirWriter()
+
+        debug_info = DebugInfo(consumer.uuid, archiver, writer)
+
+        # config the archive
+        # collect the archive
+        # persist the archive
+        # move the archive
+        # cleanup up the mess
+
+        # FIXME: exception handling
+        debug_info.collect()
+        debug_info.save()
+
+
+class DebugInfo(object):
+    def __init__(self, consumer_uuid, archiver=None, destination_writer=None):
+        self.uuid = consumer_uuid
+        self.archiver = archiver
+        self.writer = destination_writer
+        self.collector = DebugInfoCollector()
+
+    def collect(self):
+        self.collector.collect()
+
+    def save(self):
+        self.archiver.save()
+
+
+class DebugInfoCollector(object):
+    def collect(self):
+        os.makedirs(self.content_path)
+
+        owner = self.cp.getOwner(self.uuid)
 
         try:
-            os.makedirs(content_path)
-
-            owner = self.cp.getOwner(consumer.uuid)
-
-            try:
-                self._write_flat_file(content_path, "subscriptions.json",
-                                      self.cp.getSubscriptionList(owner['key']))
-            except Exception, e:
-                log.warning("Server does not allow retrieval of subscriptions by owner.")
-
-            self._write_flat_file(content_path, "consumer.json",
-                                  self.cp.getConsumer(consumer.uuid))
-            self._write_flat_file(content_path, "compliance.json",
-                                  self.cp.getCompliance(consumer.uuid))
-            self._write_flat_file(content_path, "entitlements.json",
-                                  self.cp.getEntitlementList(consumer.uuid))
-            self._write_flat_file(content_path, "pools.json",
-                                  self.cp.getPoolsList(consumer.uuid, True, None, owner['key']))
-            self._write_flat_file(content_path, "version.json",
-                                  self._get_version_info())
-
-            # FIXME: we need to anon proxy passwords?
-            self._copy_directory('/etc/rhsm', content_path)
-            self._copy_directory('/var/log/rhsm', content_path)
-            self._copy_directory('/var/lib/rhsm', content_path)
-            self._copy_directory(cfg.get('rhsm', 'productCertDir'), content_path)
-            self._copy_directory(cfg.get('rhsm', 'entitlementCertDir'), content_path)
-            self._copy_directory(cfg.get('rhsm', 'consumerCertDir'), content_path)
-
-            # FIXME: this could still be in /tmp
-            # FIXME: destination is user input from a trusted user, but it
-            # could still be something dumb
-            print "destination", self.options.destination
-            if not os.path.exists(self.options.destination):
-                os.makedirs(self.options.destination)
-
-            # build an archive by default
-            if self.options.archive:
-                os.makedirs(tar_path)
-                tar_file_path = os.path.join(tar_path, "tmp-system.tar")
-                print "tar_path", tar_path, tar_file_path
-                tf = tarfile.open(tar_file_path, "w:gz")
-                # FIXME: full name
-                tf.add(content_path, code)
-                tf.close()
-                final_path = os.path.join(self.options.destination, "system-debug-%s.tar.gz" % code)
-                print "final_path", final_path
-
-                # FIXME: move securely
-                # FIXME: perms should be 0600, only root can read
-                shutil.move(tar_file_path, final_path)
-                print _("Wrote: %s") % final_path
-            else:
-                # FIXME: need to do this securely
-                shutil.move(content_path, self.options.destination)
-
-                print _("Wrote: %s/%s") % (self.options.destination, code)
-
+            self._write_flat_file(content_path, "subscriptions.json",
+                                    self.cp.getSubscriptionList(owner['key']))
         except Exception, e:
-            managercli.handle_exception(_("Unable to create zip file of system information: %s") % e, e)
-            sys.exit(-1)
-        finally:
-            if assemble_path and os.path.exists(assemble_path):
-                shutil.rmtree(assemble_path, True)
+            log.warning("Server does not allow retrieval of subscriptions by owner.")
 
-    def _make_code(self):
-        return datetime.now().strftime("%Y%m%d-%f")
+        # self.archive.add_json("consumer", self.cp.getConsumer(self.uuid))
+        self._write_flat_file(content_path, "consumer.json",
+                                self.cp.getConsumer(self.uuid))
+        self._write_flat_file(content_path, "compliance.json",
+                                self.cp.getCompliance(self.uuid))
+        self._write_flat_file(content_path, "entitlements.json",
+                                self.cp.getEntitlementList(self.uuid))
+        self._write_flat_file(content_path, "pools.json",
+                                self.cp.getPoolsList(self.uuid, True, None, owner['key']))
+        self._write_flat_file(content_path, "version.json",
+                                self._get_version_info())
+
+        # FIXME: we need to anon proxy passwords?
+        #self.add_dir("/etc/rhsm")
+        self._copy_directory('/etc/rhsm', content_path)
+        self._copy_directory('/var/log/rhsm', content_path)
+        self._copy_directory('/var/lib/rhsm', content_path)
+        self._copy_directory(cfg.get('rhsm', 'productCertDir'), content_path)
+        self._copy_directory(cfg.get('rhsm', 'entitlementCertDir'), content_path)
+        self._copy_directory(cfg.get('rhsm', 'consumerCertDir'), content_path)
+
+        # FIXME: this could still be in /tmp
+        # FIXME: destination is user input from a trusted user, but it
+        # could still be something dumb
+        print "destination", self.options.destination
+        if not os.path.exists(self.options.destination):
+            os.makedirs(self.options.destination)
 
     def _get_version_info(self):
         return {"server type": self.server_versions["server-type"],
                 "subscription management server": self.server_versions["candlepin"],
                 "subscription-manager": self.client_versions["subscription-manager"],
                 "python-rhsm": self.client_versions["python-rhsm"]}
+
+
+class SaferFileCopy(object):
+    def move(self, src, dst):
+        self.copy(src, dst)
+        os.unlink(src)
+
+    def copy(self, src, dst):
+        pass
+        # open src, open dst O_EXCL
+        # base on shutil.copyfile/copyfileobj
+
+
+class DebugInfoTarWriter(object):
+    def save(self):
+        os.makedirs(tar_path)
+        tar_file_path = os.path.join(tar_path, "tmp-system.tar")
+        print "tar_path", tar_path, tar_file_path
+        tf = tarfile.open(tar_file_path, "w:gz")
+        # FIXME: full name
+        tf.add(content_path, code)
+        tf.close()
+        dest_path = os.path.join(self.options.destination, "system-debug-%s.tar.gz" % code)
+        print "dest_path", dest_path
+
+        self._move(dest_path)
+
+    def _move(self, dest_path):
+        # FIXME: move securely
+        # FIXME: perms should be 0600, only root can read
+        # Okay if the dst dir is someplace we can create a file "securely"
+        #   (without a less priv user/group potentially racing us to the
+        #   filename), but if say, /tmp, we create the file using mkstemp
+
+        # If we are just writing a file, we can try it O_EXCL and copy the
+        # contents
+        shutil.move(tar_file_path, dest_path)
+        print _("Wrote: %s") % dest_path
+
+
+class DebugInfoDirWriter(object):
+    def save(self):
+        self._move()
+
+    def _move(self):
+        # FIXME: need to do this securely
+        shutil.move(content_path, self.options.destination)
+
+        print _("Wrote: %s/%s") % (self.options.destination, code)
+
+
+class DebugInfoArchiveConfig(object):
+    def __init__(self):
+        self.assemble_path = ASSEMBLE_DIR
+        self.archive_name = self._gen_archive_name()
+        self.content_path = os.path.join(self.assemble_path, self.archive_name)
+        self.tar_path = os.path.join(self.assemble_path, "tar")
+
+    def _gen_archive_name(self):
+        return datetime.now().strftime("%Y%m%d-%f")
+
+
+class DebugInfoArchiver(object):
+
+    def __init__(self, archive_config):
+        self.config = archive_config
+
+    def add_json(self, name, json_blob):
+        self._write_flat_file(self.config.content_path,
+                              "%s.json" % name,
+                              json_blob)
+
+    def add_dir(self, dir_path):
+        self._copy_directory(dir_path, self.config.content_path)
+
+    def cleanup(self):
+        #FIXME: handle errors
+        os.unlink(self.config.content_path)
+        os.unlink(self.config.tar_path)
 
     def _write_flat_file(self, content_path, filename, content):
         path = os.path.join(content_path, filename)
@@ -152,6 +231,3 @@ class SystemCommand(CliCommand):
         if os.path.isabs(src_path):
             rel_path = src_path[1:]
         shutil.copytree(src_path, os.path.join(dest_path, rel_path))
-
-    def _get_assemble_dir(self):
-        return ASSEMBLE_DIR
