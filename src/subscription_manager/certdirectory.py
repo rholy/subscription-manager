09119631575757e2285301bd48d96c6f3633509d
Adrian Likins <alikins@redhat.com>
2014-04-21 18:15:29 -0400

    move find_[all_]by_product methods to prod/ent dir
    Nothing uses these directory from CertificateDirectory, so
    move them to the approriate subclasses.

--- src/subscription_manager/certdirectory.py
+++ src/subscription_manager/certdirectory.py
@@ -150,40 +150,6 @@ class CertificateDirectory(Directory):
                 return c
         return None
 
-    def find_all_by_product(self, p_hash):
-        certs = set()
-        providing_stack_ids = set()
-        stack_id_map = {}
-        for c in self.list():
-            for p in c.products:
-                if p.id == p_hash:
-                    certs.add(c)
-                    # Keep track of stacks that provide our product
-                    if (c.order and c.order.stacking_id):
-                        providing_stack_ids.add(c.order.stacking_id)
-
-            # Keep track of stack ids in case we need them later.  avoids another loop
-            if (c.order and c.order.stacking_id):
-                if c.order.stacking_id not in stack_id_map:
-                    stack_id_map[c.order.stacking_id] = set()
-                stack_id_map[c.order.stacking_id].add(c)
-
-        # Complete
-        for stack_id in providing_stack_ids:
-            certs |= stack_id_map[stack_id]
-
-        return list(certs)
-
-    def find_by_product(self, p_hash):
-        for c in self.list():
-            for p in c.products:
-                if p.id == p_hash:
-                    return c
-        return None
-
-    #Set up an alias for backwards compatibility
-    findByProduct = find_by_product
-
 
 class ProductDirectory(CertificateDirectory):
 
@@ -212,6 +178,16 @@ class ProductDirectory(CertificateDirectory):
             installed_products[product.id] = product_cert
         log.debug("Installed product IDs: %s" % installed_products.keys())
         return installed_products
+
+    def find_by_product(self, p_hash):
+        for c in self.list():
+            for p in c.products:
+                if p.id == p_hash:
+                    return c
+        return None
+
+    #Set up an alias for backwards compatibility
+    findByProduct = find_by_product
 
 
 class ConsumerIdentityDirectory(CertificateDirectory):
@@ -287,6 +263,29 @@ class EntitlementDirectory(CertificateDirectory):
                     entitlements.append(cert)
         return entitlements
 
+    def find_all_by_product(self, p_hash):
+        certs = set()
+        providing_stack_ids = set()
+        stack_id_map = {}
+        for c in self.list():
+            for p in c.products:
+                if p.id == p_hash:
+                    certs.add(c)
+                    # Keep track of stacks that provide our product
+                    if (c.order and c.order.stacking_id):
+                        providing_stack_ids.add(c.order.stacking_id)
+
+            # Keep track of stack ids in case we need them later.  avoids another loop
+            if (c.order and c.order.stacking_id):
+                if c.order.stacking_id not in stack_id_map:
+                    stack_id_map[c.order.stacking_id] = set()
+                stack_id_map[c.order.stacking_id].add(c)
+
+        # Complete
+        for stack_id in providing_stack_ids:
+            certs |= stack_id_map[stack_id]
+
+        return list(certs)
 
 class Path:
 
