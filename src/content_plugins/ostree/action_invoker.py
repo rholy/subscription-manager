66a399765c02e0b086814957f70ffc9944cfe36f
Adrian Likins <alikins@redhat.com>
2014-05-06 19:18:18 -0400

    -wip

--- src/content_plugins/ostree/action_invoker.py
+++ src/content_plugins/ostree/action_invoker.py
@@ -12,16 +12,24 @@
 # granted to use or replicate Red Hat trademarks that are incorporated
 # in this software or its documentation.
 #
+import logging
 
 from subscription_manager import api
 
+from content_plugins.ostree import repo_file
+from content_plugins.ostree import model
 
-class OstreeContentActionInvoker(object):
+# plugins get
+log = logging.getLogger('rhsm-app.' + __name__)
+
+
+class OstreeContentActionInvoker(api.BaseActionInvoker):
     def __init__(self):
         self.report = None
 
-    def update(self):
-        print "ostree content update"
+    def _do_update(self):
+        action = OstreeContentUpdateActionCommand()
+        return action.perform()
 
 
 class OstreeContentUpdateActionCommand(object):
@@ -33,6 +41,31 @@ class OstreeContentUpdateActionCommand(object):
     """
     def __init__(self):
         self.report = OstreeContentUpdateActionReport()
+
+        # starting state of ostree config
+        self.ostree_config = model.OstreeConfig()
+
+    def perform(self):
+        # define... somewhere?
+        OSTREE_CONTENT_TYPE = "ostree"
+        self.ostree_config.load()
+
+        # bleah, just do it
+        ent_dir = api.require(api.ENT_DIR)
+
+        content_set = set()
+        # valid ent certs could be an iterator
+        for ent_cert in ent_dir.list_valid():
+            # ditto content
+            for content in ent_cert.content:
+                log.debug("content: %s" % content)
+
+                if content.content_type == OSTREE_CONTENT_TYPE:
+                    log.debug("adding %s to ostree content" % content)
+                    content_set.add(content)
+
+        for content in content_set:
+            log.debug("Do a thing to content: %s" % content)
 
 
 class OstreeContentUpdateActionReport(api.ActionReport):
