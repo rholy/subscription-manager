bab1f1b7027e5baf33c39856d72f78cd10fd10ff
Adrian Likins <alikins@redhat.com>
2014-05-07 18:15:43 -0400

    Trying to bake in more flexibility.

--- src/subscription_manager/api.py
+++ src/subscription_manager/api.py
@@ -2,3 +2,5 @@
 from subscription_manager import base_plugin
 from subscription_manager.certlib import ActionReport
 from subscription_manager.certlib import BaseActionInvoker
+
+from subscription_manager import injection as inj
